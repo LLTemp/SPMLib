8cce9ca2cf53b65f738697c00ad64f9259976652
sergeybrazhnik <6epreu@gmail.com>
2021-02-26 23:02:46 +0300

    - up

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "SDKLib",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(name: "SDKLib", targets: ["UnitTestWithPod"]),
+        .library(name: "SDKLib", targets: ["UnitTestWithPod", "EmptyToSupportDependencies"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -17,6 +17,12 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+                name: "EmptyToSupportDependencies",
+                dependencies: [
+                    .product(name: "GRPC", package: "grpc-swift"),
+                    .product(name: "CGRPCZlib", package: "grpc-swift")
+                ]),
         .binaryTarget(
                 name: "UnitTestWithPod",
                 url: "https://testbuildartefacts.s3.us-east-2.amazonaws.com/sic-sdk-ios/1.2.3/UnitTestWithPod.xcframework.zip",
